cd995903cb830f82473c35db40f13c315e94ab2d
Amaldudezzz <64975546+Amaldudezzz@users.noreply.github.com>
2021-07-01 11:14:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,4 +29,4 @@ apt-get install -y nodejs libwebp ffmpeg wget tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
  
-echo '©2021• Alphabot All dependencies have been installed, please run the command "cd" --> "cd BOTZ2" --> "npm start" to immediately start the script'
+echo '©2021• AK BOT All dependencies have been installed, please run the command "cd" --> "cd AK-BOT" --> "npm start" to immediately start the script'
